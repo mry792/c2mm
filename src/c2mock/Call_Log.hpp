0e31c8554fba79c2cbd08c6cedc905b9a4bc3356
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2022-10-15 00:20:35 -0500

    feature: add class Call_Log

---
+++ src/c2mock/Call_Log.hpp
@@ -0,0 +1,82 @@
+#ifndef C2MOCK__CALL_LOG_HPP_
+#define C2MOCK__CALL_LOG_HPP_
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace c2mock {
+/**
+ * Represents a log of calls to some function.
+ *
+ * @tparam T_Arg_Tuple A @c std::tuple representing the arguments to the
+ *     function in question.
+ */
+template <typename T_Arg_Tuple>
+class Call_Log {
+  public:
+    using Arg_Tuple = T_Arg_Tuple;
+    using Call_List = std::vector<std::unique_ptr<Arg_Tuple const>>;
+
+    /**
+     * Read-only accessor for the unconsumed calls logged with this object.
+     */
+    Call_List const& calls () const { return calls_; }
+
+    /**
+     * Log a call with this object. This owns the new argument objects.
+     * @param[in] args Arguments of the call to log.
+     */
+    template <typename... Args>
+    void log (Args&&... args) {
+        calls_.emplace_back(
+            std::make_unique<Arg_Tuple>(
+                std::forward<Args>(args)...
+            )
+        );
+    }
+
+    /**
+     * Consume the first call found that matches @p matcher. This will remove it
+     * from the @c Call_Log.
+     * @param[in] matcher A matcher than can accept @p T_Arg_Tuple.
+     * @return @c true if a call was found to match. Else returns @c false.
+     */
+    template <typename T_Arg_Matcher>
+    bool consume_match (T_Arg_Matcher const& matcher) {
+        auto match = [&matcher] (auto const& call) {
+            return matcher.match(*call);
+        };
+        auto iter = std::ranges::find_if(calls_, match);
+
+        if (iter == calls_.end()) {
+            return false;
+        }
+
+        calls_.erase(iter);
+        return true;
+    }
+
+    /**
+     * Verifies that no unconsumed calls remain logged with this instance.
+     *
+     * This should only be called as part of a Catch2 @c TEST_CASE. It is a
+     * check-style verification where failure fails the test but continues
+     * executing.
+     */
+    void check_no_calls () const {
+        for (auto const& call : calls_) {
+            (void)call;
+            // TODO(emery): print out function name and arguments
+            FAIL_CHECK("unconsumed call");
+        }
+    }
+
+  private:
+    Call_List calls_;
+};
+}  // namespace c2mock
+
+#endif  // C2MOCK__CALL_LOG_HPP_
---
+++ src/c2mock/Call_Log.test.cpp
@@ -0,0 +1,55 @@
+#include "c2mock/Call_Log.hpp"
+
+#include <tuple>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "c2mock/matchers/Tuple_Matcher.hpp"
+
+SCENARIO (
+    "If all calls are consumed, Call_Log::check_no_calls() doesn't fail."
+) {
+    GIVEN ("a Call_Log") {
+        c2mock::Call_Log<std::tuple<int>> call_log{};
+
+        WHEN ("some calls are logged") {
+            call_log.log(7);
+            call_log.log(-4);
+
+            AND_WHEN ("those calls are all matched") {
+                using c2mock::matchers::matches;
+                CHECK(call_log.consume_match(matches(std::tuple{-4})));
+                CHECK(not call_log.consume_match(matches(std::tuple{4})));
+                CHECK(call_log.consume_match(matches(std::tuple{7})));
+
+                THEN ("Call_Log::check_no_calls() doesn't fail") {
+                    call_log.check_no_calls();
+                }
+            }
+        }
+    }
+}
+
+SCENARIO (
+    "If some calls are unconsumed, Call_Log::check_no_calls() fails.",
+    "[!shouldfail]"
+) {
+    GIVEN ("a Call_Log") {
+        c2mock::Call_Log<std::tuple<int>> call_log{};
+
+        WHEN ("some calls are logged") {
+            call_log.log(7);
+            call_log.log(-4);
+            call_log.log(2);
+
+            AND_WHEN ("not all calls are matched") {
+                using c2mock::matchers::matches;
+                CHECK(call_log.consume_match(matches(std::tuple{-4})));
+
+                THEN ("Call_Log::check_no_calls() fails") {
+                    call_log.check_no_calls();
+                }
+            }
+        }
+    }
+}
