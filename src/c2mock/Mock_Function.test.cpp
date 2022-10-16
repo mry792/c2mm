b43d9e7ec1a90337991c69d9b699714a916b6343
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2022-10-16 09:33:28 -0500

    feature: add basic class Mock_Function

---
+++ src/c2mock/Mock_Function.test.cpp
@@ -0,0 +1,61 @@
+#include "c2mock/Mock_Function.hpp"
+
+#include <optional>
+#include <tuple>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "c2mock/matchers/Comparison_Matcher.hpp"
+
+SCENARIO ("If all calls are consumed, Mock_Function doesn't fail.") {
+    GIVEN ("a Mock_Function") {
+        using c2mock::Mock_Function;
+        auto func_ptr = std::make_unique<Mock_Function<void(int, double)>>();
+        auto& func = *func_ptr;
+
+        WHEN ("some calls are logged") {
+            func(7, 4.2);
+            func(-4, 1.1);
+
+            AND_WHEN ("those calls are all matched") {
+                using c2mock::matchers::less_than;
+                func.check_called(less_than(8), 4.2);
+                func.require_called(-4, less_than(2));
+
+                THEN ("Mock_Function::~Mock_Function() doesn't fail") {
+                    func_ptr.reset();
+                }
+            }
+        }
+    }
+}
+
+SCENARIO ("Mock_Function fails when calls aren't matched.", "[!shouldfail]") {
+    GIVEN ("a Mock_Function") {
+        using c2mock::Mock_Function;
+        auto func_ptr = std::make_unique<Mock_Function<void(int, double)>>();
+        auto& func = *func_ptr;
+
+        WHEN ("some calls are logged") {
+            func(2.2, 4);
+            func(3.3, -2);
+
+            AND_WHEN ("not all calls are matched") {
+                using c2mock::matchers::greater_than;
+                func.check_called(greater_than(2), greater_than(0));
+
+                THEN ("Mock_Function::~Mock_Function() fails") {
+                    func_ptr.reset();
+                }
+            }
+
+            THEN ("unmatched .check_called() fails") {
+                func.check_called(0, 0);
+            }
+
+            THEN ("unmatched .require_called() fails") {
+                func.require_called(0, 0);
+            }
+        }
+    }
+}
