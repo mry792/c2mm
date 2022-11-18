a86fd47dd71f2588527df7a92041735b2cde5d72
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2022-11-17 21:54:52 -0600

    refactor(mock): factor out common logic of Mock_Function's check_called and require_called functions

--- src/c2mm/mock/Mock_Function.hpp
+++ src/c2mm/mock/Mock_Function.hpp
@@ -10,6 +10,8 @@
 #include "c2mm/mock/Call_Log.hpp"
 #include "c2mm/mock/Default_Action.hpp"
 #include "c2mm/mock/args.hpp"
+#include "c2mm/mock/reporters/Fail.hpp"
+#include "c2mm/mock/reporters/Fail_Check.hpp"
 
 namespace c2mm::mock {
 /**
@@ -81,14 +83,38 @@ class Mock_Function<T_Return(T_Parameters...)> {
     /**
      * Check for a past call whose arguments match @p arg_constraints.
      *
-     * Checks each unconsumed logged call in turn until one matches the given @p
+     * Checks each unconsumed logged call in turn until one matches the given @c
      * arg_constraints. For each constraint that is a matcher, the comparison
      * is equivalent to `constraint.match(value)`. Otherwise, the comparison is
      * `operator ==`. All comparisons must resolve to @c true for a call to be
      * matched.
      *
      * Once a call is matched, it is consumed and cannot match another set of
-     * constraints in a future call.
+     * constraints in a future validation.
+     *
+     * This is a lower level function that is typically not used by users of
+     * this library. Prefer one of `check_called` or `require_called` bellow.
+     *
+     * @param[out] reporter Callable used to report a failure to find a logged
+     *     call that matches the constraints.
+     * @param[in] arg_constraints Constraints to check against arguments of
+     *     logged calls.
+     */
+    template <typename T_Reporter, typename... T_Constraints>
+    auto validate_called (
+        T_Reporter& reporter,
+        T_Constraints const&... arg_constraints
+    ) {
+        auto matcher = matchers::matches(bind_args(arg_constraints...));
+
+        if (not calls_.consume_match(matcher)) {
+            // TODO(emery): Print out constraints.
+            reporter("No call whose arguments match.");
+        }
+    }
+
+    /**
+     * Check for a past call whose arguments match @p arg_constraints.
      *
      * If no call matches @p arg_constraints this is effectively a failed Catch2
      * CHECK. It fails the test but continues executing.
@@ -97,30 +123,11 @@ class Mock_Function<T_Return(T_Parameters...)> {
      */
     template <typename... T_Constraints>
     void check_called (T_Constraints const&... arg_constraints) {
-        static_assert(
-            std::is_same_v<T_Return, void>,
-            "This function is only available for void-return mock functions."
-        );
-
-        auto matcher = matchers::matches(bind_args(arg_constraints...));
-
-        if (not calls_.consume_match(matcher)) {
-            // TODO(emery): Print out matchers.
-            FAIL_CHECK("No call whose arguments match.");
-        }
+        validate_called(reporters::Fail_Check{}, arg_constraints...);
     }
 
     /**
      * Check for a past call whose arguments match @p arg_constraints.
-     *
-     * Checks each unconsumed logged call in turn until one matches the given @p
-     * arg_constraints. For each constraint that is a matcher, the comparison
-     * is equivalent to `constraint.match(value)`. Otherwise, the comparison is
-     * `operator ==`. All comparisons must resolve to @c true for a call to be
-     * matched.
-     *
-     * Once a call is matched, it is consumed and cannot match another set of
-     * constraints in a future call.
      *
      * If no call matches @p arg_constraints this is effectively a failed Catch2
      * REQUIRE. It fails the test stops execution.
@@ -129,17 +136,7 @@ class Mock_Function<T_Return(T_Parameters...)> {
      */
     template <typename... T_Constraints>
     void require_called (T_Constraints const&... arg_constraints) {
-        static_assert(
-            std::is_same_v<T_Return, void>,
-            "This function is only available for void-return mock functions."
-        );
-
-        auto matcher = matchers::matches(bind_args(arg_constraints...));
-
-        if (not calls_.consume_match(matcher)) {
-            // TODO(emery): Print out matchers.
-            FAIL("No call whose arguments match.");
-        }
+        validate_called(reporters::Fail{}, arg_constraints...);
     }
 
   private:
